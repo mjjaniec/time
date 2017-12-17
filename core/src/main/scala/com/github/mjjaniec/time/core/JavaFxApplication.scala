1814a6fbbb521c5ff4660f7c87c426cf646b7ad0
Michał Janiec <mjjaniec@gmail.com>
2017-12-17 17:23:45 +0100

    use LF instead of CRLF

--- core/src/main/scala/com/github/mjjaniec/time/core/JavaFxApplication.scala
+++ core/src/main/scala/com/github/mjjaniec/time/core/JavaFxApplication.scala
@@ -8,7 +8,7 @@ import com.github.plushaze.traynotification.notification.{Notifications, TrayNot
 class JavaFxApplication extends Application {
 
   override def start(primaryStage: Stage): Unit = {
-    if (getParameters.getUnnamed.size() == 2) { 
+    if (getParameters.getUnnamed.size() == 2) {
       val version = getParameters.getUnnamed.get(0)
       val changeLog = getParameters.getUnnamed.get(1)
       Platform.runLater(() => new TrayNotification("Updated to version: " + version, changeLog, Notifications.SUCCESS).showAndWait())
